3a6e2a22f0adc32b22e5148364eb2e759c804d08
E. G. Bland <egb528@york.ac.uk>
2018-10-04 17:33:15 +0100

    Fill out FortniteAPIHandler

--- src/net/muneyamaru/fortniteapi/FortniteAPIHandler.java
+++ src/net/muneyamaru/fortniteapi/FortniteAPIHandler.java
@@ -25,10 +25,59 @@
  */
 package net.muneyamaru.fortniteapi;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import net.muneyamaru.lightcontroller.philips.HueAPIHandler;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 /**
  *
  * @author E. G. Bland
  */
 public class FortniteAPIHandler {
+    private static transient final String API_KEY = "4c81d74ba3696916a76cefad719e8bbe";
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final int HTTP_BUFFER_LENGTH = 1024;
     
+    private final String apiBase = "https://fortnite-public-api.theapinetwork.com/prod09/";
+    
+    JSONObject apiCall(String resource, JSONObject body, String method) {
+        JSONObject obj = new JSONObject();
+        try {
+            System.out.println("Requesting " + this.apiBase + "/" + resource + " with method " + method);
+            final URL url = new URL(this.apiBase + "/" + resource);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            
+            // fortnite auth stuff
+            connection.setRequestProperty("Authorization",API_KEY);
+            connection.setRequestProperty("X-Fortnite-API-Version","v1.1");
+            
+            connection.setRequestProperty("User-Agent",USER_AGENT);   
+            connection.setRequestMethod(method);
+            if(method.equals("PUT")) {
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Content-Length",Integer.toString(body.toJSONString().length()));
+                connection.getOutputStream().write(body.toJSONString().getBytes("UTF-8"));
+            }
+            
+            InputStream is = connection.getInputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buf = new byte[HTTP_BUFFER_LENGTH];
+            int n;
+            
+            while((n = is.read(buf)) != -1) baos.write(buf,0,n);
+            
+            JSONParser parser = new JSONParser();
+            Object response = parser.parse(new String(baos.toByteArray())); // TODO localise
+            obj.put("response",response);
+            
+            return obj;
+        } catch(Exception ex) { // TODO narrower exception types, handle
+            ex.printStackTrace();
+            return obj;
+        }
+    }
 }
